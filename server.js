ea468c466464b5ce90aa16587a61269f76564d99
Stuart Fong <stuartfong@Admins-MacBook-Pro.local>
2020-09-17 23:03:41 -0400

    pls dont use my creds

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { promisify } = require("util")
 
 const app = express()
 
-const creds = require(JSON.parse(process.env.GCP_KEY_FILE))
+const creds = require("./client_secret.json")
 require('dotenv').config()
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")))
